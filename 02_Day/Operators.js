67eafa01a50df7bd03dbe205c13e7388a85549f1
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-25 01:27:39 -0400

    Add the JavaScript solution

---
+++ 02_Day/Operators.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    var tip = meal_cost * tip_percent / 100;
+    var tax = meal_cost * tax_percent / 100;
+    var total = meal_cost + tip + tax;
+    console.log(total.toFixed())
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
