38af02f3066b024ce0d1bcf42ff62a7d34798da6
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-25 14:41:23 -0400

    Add the Python solution

--- 08_Day/DicMap.py
+++ 08_Day/DicMap.py
@@ -1,1 +1,17 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+data = [line.rstrip() for line in sys.stdin.readlines()]
+n = int(data[0])
+dic = {}
+
+for d in data[1:n+1]:
+    k = d.split(' ')[0]
+    v = d.split(' ')[1]
+    dic[k] = v
+
+for t in data[n+1:]:
+    if t in dic.keys():
+        print(t + '=' + dic[t])
+    else:
+        print('Not found')
