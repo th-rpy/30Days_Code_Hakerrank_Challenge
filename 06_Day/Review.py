c18685c32df0b9aeb5bf11479305e6b990bb47c8
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-25 13:06:40 -0400

    Add the Python solution

---
+++ 06_Day/Review.py
@@ -0,0 +1,22 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+data = [line.rstrip().split()[0] for line in sys.stdin.readlines()]
+
+def even_odd_indexed(string):
+    even_indexed = ''
+    odd_indexed = ''
+    for i in range(0, len(string)):
+        if i%2 == 0:
+            even_indexed+=string[i]
+        else:
+            odd_indexed+=string[i]
+    print(even_indexed + ' ' + odd_indexed)
+
+
+test_cases = data[1:]
+for test in test_cases:
+    even_odd_indexed(test)
+
+
+
