28803a84c0adfa998d659d278cd1a086c610791e
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-26 01:31:41 -0400

    Optimize  the Python solution

--- 29_Day/BitwiseAND.js
+++ 29_Day/BitwiseAND.js
@@ -33,8 +33,76 @@ function readLine() {
 
 function bitwiseAnd(N, K) {
     // Write your code here
-    
+    let r = ((K-1) || k ) <= N ? K-1 : K-2
+    return r
 
+}
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'bitwiseAnd' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER N
+ *  2. INTEGER K
+ */
+
+function bitwiseAnd(N, K) {
+    // Write your code here
+    var max = 0;
+    for (let b = 0; b <= N; b++) {
+        for (let a = 0; a < b; a++) {
+            let ab = a&b;
+            if (ab < K && ab > max) {
+                max = ab;
+            }
+        }
+    }
+ 
+    return max;
+
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const count = parseInt(firstMultipleInput[0], 10);
+
+        const lim = parseInt(firstMultipleInput[1], 10);
+
+        const res = bitwiseAnd(count, lim);
+
+        ws.write(res + '\n');
+    }
+
+    ws.end();
 }
 
 function main() {
