61cf98ed9af20c83bc5ec1d073ce68c2fdd375b1
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-25 22:49:07 -0400

    Add the Javascript solution

--- 15_Day/LinkedList.js
+++ 15_Day/LinkedList.js
@@ -23,7 +23,22 @@ function Node(data){
 function Solution(){
 
 	this.insert=function(head,data){
-          //complete this method
+        //complete this method
+        let node = new Node(data);
+    
+        if (!head) {
+            head = node;
+        } else {
+            let current = head;
+    
+            while (current.next != null) {
+                current = current.next;
+            }
+    
+            current.next = node;
+        }
+    
+        return head;
     };
 
 	this.display=function(head){
